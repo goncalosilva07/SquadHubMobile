a77cd006d1bb40088e02ff08482f8080e64af90a
GoncaloSilva78 <goncalosilva78@ua.pt>
2024-12-24 19:49:53 +0000

    +game statistics

--- SquadHub/app/src/main/java/com/example/squadhub/GameStatisticsActivity.kt
+++ SquadHub/app/src/main/java/com/example/squadhub/GameStatisticsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -75,8 +76,45 @@ class GameStatisticsActivity : AppCompatActivity() {
 
                     if (gameStatistics != null){
                         findViewById<TextView>(R.id.gamestatistics_nostatistics).visibility = View.GONE
-                    }else{
 
+                        val possessionBar = findViewById<ProgressBar>(R.id.possession_bar)
+                        val possession_txt = findViewById<TextView>(R.id.possession_txt)
+                        //val team1Name = findViewById<TextView>(R.id.team1_name)
+                        //val team2Name = findViewById<TextView>(R.id.team2_name)
+
+                        // Exemplo de percentagens
+                        val possessionTeam1 = gameStatistics.getInt("ball_posession") // Percentagem de posse para a Equipa 1
+                        val possessionTeam2 = 100 - possessionTeam1
+
+                        // Atualizar o texto e a barra
+                        //team1Name.text = "Equipa 1: $possessionTeam1%"
+                        //team2Name.text = "Equipa 2: $possessionTeam2%"
+                        possessionBar.progress = possessionTeam1
+                        possession_txt.text = possessionTeam1.toString() + "%"
+
+                        findViewById<TextView>(R.id.goals_scored).text = gameStatistics.getInt("goals_scored").toString()
+                        findViewById<TextView>(R.id.goals_conceded).text = gameStatistics.getInt("goals_conceded").toString()
+                        findViewById<TextView>(R.id.shots).text = gameStatistics.getInt("shots").toString()
+                        findViewById<TextView>(R.id.shots_goal).text = gameStatistics.getInt("shots_goal").toString()
+                        findViewById<TextView>(R.id.passes).text = gameStatistics.getInt("passes").toString()
+                        findViewById<TextView>(R.id.fouls).text = gameStatistics.getInt("fouls").toString()
+
+                    }else{
+                        findViewById<TextView>(R.id.ballPossessionTxt).visibility = View.GONE
+                        findViewById<ProgressBar>(R.id.possession_bar).visibility = View.GONE
+                        findViewById<TextView>(R.id.possession_txt).visibility = View.GONE
+                        findViewById<TextView>(R.id.goalsScoredTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.goalsConcededTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.goals_scored).visibility = View.GONE
+                        findViewById<TextView>(R.id.goals_conceded).visibility = View.GONE
+                        findViewById<TextView>(R.id.shotsTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.shotsGoalTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.shots).visibility = View.GONE
+                        findViewById<TextView>(R.id.shots_goal).visibility = View.GONE
+                        findViewById<TextView>(R.id.passesTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.foulsTxt).visibility = View.GONE
+                        findViewById<TextView>(R.id.passes).visibility = View.GONE
+                        findViewById<TextView>(R.id.fouls).visibility = View.GONE
                     }
 
                 } catch (e: JSONException) {
