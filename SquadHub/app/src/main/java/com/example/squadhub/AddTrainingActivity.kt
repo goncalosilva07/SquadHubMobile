181e02113b0cb21769ead994b572954f13a727fc
GoncaloSilva78 <goncalosilva78@ua.pt>
2025-01-04 01:45:06 +0000

    +add training +revisão titulos das páginas

--- SquadHub/app/src/main/java/com/example/squadhub/AddTrainingActivity.kt
+++ SquadHub/app/src/main/java/com/example/squadhub/AddTrainingActivity.kt
@@ -1,17 +1,33 @@
 package com.example.squadhub
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddTrainingActivity : AppCompatActivity() {
+
+    var selectedItem = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,7 +51,7 @@ class AddTrainingActivity : AppCompatActivity() {
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedItem = parent?.getItemAtPosition(position).toString()
+                selectedItem = parent?.getItemAtPosition(position).toString()
                 Toast.makeText(this@AddTrainingActivity, selectedItem, Toast.LENGTH_LONG).show()
             }
 
@@ -44,5 +60,123 @@ class AddTrainingActivity : AppCompatActivity() {
             }
         }
 
+        findViewById<ImageView>(R.id.calendar).setOnClickListener {
+            // Obter a data atual
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            // Criar e exibir o DatePickerDialog
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                findViewById<EditText>(R.id.addTraining_date).setText(selectedDate)
+                Toast.makeText(this, selectedDate, Toast.LENGTH_SHORT).show()
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+
+        findViewById<ImageView>(R.id.startTime).setOnClickListener {
+            // Abrir o TimePickerDialog
+            val timePicker = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
+                // Formatar a hora como HH:mm
+                val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                Toast.makeText(this, selectedTime, Toast.LENGTH_SHORT).show()
+                // Definir o texto no EditText
+                findViewById<EditText>(R.id.addTraining_startTime).setText(selectedTime)
+            }, 12, 0, true) // O último parâmetro 'true' indica formato 24h, use 'false' para 12h.
+
+            timePicker.show()
+        }
+
+        findViewById<ImageView>(R.id.endTime).setOnClickListener {
+            // Abrir o TimePickerDialog
+            val timePicker = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
+                // Formatar a hora como HH:mm
+                val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                Toast.makeText(this, selectedTime, Toast.LENGTH_SHORT).show()
+                // Definir o texto no EditText
+                findViewById<EditText>(R.id.addTraining_endTime).setText(selectedTime)
+            }, 12, 0, true) // O último parâmetro 'true' indica formato 24h, use 'false' para 12h.
+
+            timePicker.show()
+        }
+
     }
+
+    fun goBack(view: View){
+        val intent = Intent(this, TrainingSessionActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun addTraining(view: View){
+
+        val dateString = findViewById<EditText>(R.id.addTraining_date).text.toString()
+        val startTimeString = findViewById<EditText>(R.id.addTraining_startTime).text.toString()
+        val endTimeString = findViewById<EditText>(R.id.addTraining_endTime).text.toString()
+
+        if (selectedItem != "" && dateString != "" && startTimeString != "" && endTimeString != ""){
+
+            val inputFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+            // Converte a data
+            val date = inputFormat.parse(dateString)
+            val convertedDate = outputFormat.format(date!!)
+
+            val url = Config.url +  "route.php"
+            // Criar os dados JSON
+            val jsonBody = JSONObject()
+            try {
+                jsonBody.put("route", "addTrainingSession")
+                jsonBody.put("trainingType", selectedItem)
+                jsonBody.put("date", convertedDate)
+                jsonBody.put("startTime", startTimeString)
+                jsonBody.put("endTime", endTimeString)
+                Config.club?.let { jsonBody.put("idClub", it.id) }
+                jsonBody.put("idUser", Config.idUser)
+                jsonBody.put("jwt", Core.getToken(this))
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            // Criar o JsonObjectRequest
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.POST,  // Método HTTP
+                url,  // URL
+                jsonBody,  // Dados JSON a enviar
+                { response ->
+                    // Sucesso
+                    try {
+                        //val status = response.getString("status")
+                        val message = response.getString("message")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, TrainingSessionActivity::class.java)
+                        startActivity(intent)
+
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                { error ->
+                    // Erro
+                    println("Erro na requisição: " + error.message)
+
+                    // Verificando se o erro tem um corpo de resposta
+                    val errorMessage = String(error.networkResponse.data)
+                    val jsonError = JSONObject(errorMessage)
+
+                    // Exibir a mensagem de erro enviada pela API
+                    val message = jsonError.optString("message", "Erro desconhecido")
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+            )
+            // Adicionar à fila de requisições
+            Volley.newRequestQueue(this).add(jsonObjectRequest)
+        }
+    }
+
 }
